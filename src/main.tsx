4ae50bc0b4d5d49086c8593b96be9187714abd86
Yazan Joureah <yazanzzxzz@gmail.com>
2026-08-29 20:36:27 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,9 @@ import { ToastProvider } from './context/ToastContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <BrowserRouter>
       <ToastProvider>
         <App />
       </ToastProvider>
     </BrowserRouter>
-  </React.StrictMode>,
 )
